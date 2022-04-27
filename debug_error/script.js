2055afd90fabc3dce46e085cf73e2612b58f174a
LeonarMS <leonardomscudlarek@gmail.com>
2022-04-26 21:45:26 -0300

    solução atividade, descrita em atividade.md

---
+++ debug_error/script.js
@@ -0,0 +1,18 @@
+function evalArr (arr, len) {
+    if (!arr || !len) throw new ReferenceError('Missing argument');
+    if (typeof len !== 'number') throw new TypeError(len + ', ' + typeof len + ' is not a number');
+    if (!(arr instanceof Object)) throw new TypeError('Not an Array'); 
+
+    if (arr.length == len) return arr;
+    throw new RangeError('Array.length is greater than ' + len);
+}
+
+function tryEval (arr, len) {
+    try {
+       return evalArr(arr, len);
+    } catch(e) {
+        throw e
+    }
+}
+
+console.log(tryEval([1,2,3], 4))
